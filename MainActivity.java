3c5fdc2ad12842180c8bb8fc2591221b36c2877d
paulAriri <ariripaulcoc@gmail.com>
2020-01-30 17:32:01 -0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.paularirihelloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView2)).setTextColor(
+                    getResources().getColor(R.color.colorPrimary));
+            }
+        });
+    }
+}
